cfd3bfeebf606a6341c9135fe4cb5e0a85854277
PaolaCuadros1 <paola.cuadroso@gmail.com>
2023-07-26 19:48:37 -0500

    Finished the search of the own publications.

--- src/message/service.js
+++ src/message/service.js
@@ -1,5 +1,6 @@
-import { Firestore } from "../firebase/firestore/core.js";
+import { Firestore } from "../firebase/firestore/core.js"
 import UserService from '../users/service.js'
+import { DateTime, Settings } from 'luxon'
 
 const COLLECTION_NAME = 'messages'
 
@@ -13,10 +14,23 @@ const MessageService = {
         return docRef.id
     },
 
-    async getAll(userId = '') {
+    async getAll(params = {}) {
         let query = Firestore.collection(COLLECTION_NAME).orderBy('createAt', 'desc')
-        let snapshot = await (userId != '' ? query.get() : query.where('userId', '==', userId).get())
-        //let snapshot = await Firestore.collection(COLLECTION_NAME).orderBy('createAt', 'desc').get()
+        if (typeof params === 'object' && Object.keys(params).length > 0) {
+            Object.keys(params).forEach((k) => {
+                if (k == 'createAt' && params[k] != '') {
+                    const start = new Date(`${params[k]}T05:00:00.000Z`)
+                    const end = new Date(`${params[k]}T23:59:59.000z`)
+                    query = query.where(k, '>', start)
+                    query = query.where(k, '<', end)
+                } else {
+                    if (params[k] != '') {
+                        query = query.where(k, '==', params[k])
+                    }
+                }
+            })
+        }
+        const snapshot = await query.get()
         return snapshot.docs.map(doc => doc.data())
     }
 }
